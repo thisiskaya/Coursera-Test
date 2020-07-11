578177fa1a753e5b1ac6a0a18258ec38bad5a667
Kamrun Nahar Kaya <67001360+thisiskaya@users.noreply.github.com>
2020-07-11 20:12:06 +0600

    Delete script.js

--- Module4_Sol/script.js
+++
@@ -1,8 +0,0 @@
-var array=["Keya","junaid","Suborna","Saad","Jenny"];
-var i;
-for(i=0;i<array.length;i++){
-	if(array[i].charAt(0)=="j" || array[i].charAt(0).toUpperCase()=="J")
-		{greeter2.sayGoodbye(array[i]);}
-	else
-		{greeter.sayHello(array[i]);}
-}
