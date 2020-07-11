3067f9fdbba525d60c134a89afb64ac8ad672bef
Kamrun Nahar Kaya <67001360+thisiskaya@users.noreply.github.com>
2020-07-11 20:12:37 +0600

    Add files via upload

---
+++ Module4_Soln/speakGoodbye.js
@@ -0,0 +1,12 @@
+
+(function(window){
+	var byeSpeaker={};
+	var speakWord="Good Bye ";
+	
+	byeSpeaker.speak=function(name){
+		byeSpeaker.name=name;
+		console.log(speakWord+byeSpeaker.name);
+	}
+	window.byeSpeaker=byeSpeaker;
+
+})(window);
---
+++ Module4_Soln/speakHello.js
@@ -0,0 +1,13 @@
+
+
+(function(window){
+	var helloSpeaker={};
+	var speakWord="Hello ";
+	
+	helloSpeaker.speak=function(name){
+		helloSpeaker.name=name;
+		console.log(speakWord+helloSpeaker.name);
+	}
+	window.helloSpeaker=helloSpeaker;
+
+})(window);
